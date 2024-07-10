efef772d82084368df0b5c3d68a55ab56e8344f7
Janis Schiller <schillerjanis@gmail.com>
2024-07-10 15:14:28 +0200

    add List frontend

--- frontend/src/api.ts
+++
@@ -1,4 +0,0 @@
-
-const API_URL = 'localhost:8080/api/shop'
-
-export const getShoppingList
---
+++ frontend/src/components/ShoppingList.tsx
@@ -0,0 +1,55 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Item from "./Item";
+
+interface Amount {
+    quantity: number;
+    unit: string;
+}
+
+interface Item {
+    name: string;
+    done: boolean;
+    amount: Amount;
+    category: string;
+}
+
+interface ShoppingList {
+    id: string;
+    listName: string;
+    item: Item[];
+}
+
+export default function ShoppingList() {
+    const [lists, setLists] = useState<ShoppingList[]>([]);
+
+
+    const fetchLists = () => {
+        axios.get("/api/shop")
+            .then(response => {
+                setLists(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    };
+
+    useEffect(fetchLists, [])
+
+    if(!lists) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            {lists.map(list => (
+                <div key={list.id}>
+                    <h2>{list.listName}</h2>
+
+                </div>
+            ))}
+        </div>
+    );
+
+
+}

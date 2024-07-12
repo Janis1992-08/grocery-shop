fe470874ab2f1108da312b9f6280ac8a9d879085
MarcoSkiadas <Skiadas2@hotmail.com>
2024-07-13 00:20:45 +0200

    finalize Checkbox with fetch and send it accordingly to the backend

--- backend/src/main/java/org/example/backend/service/ShopService.java
+++ backend/src/main/java/org/example/backend/service/ShopService.java
@@ -2,8 +2,11 @@ package org.example.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.backend.dto.ShoppingListDto;
+import org.example.backend.model.Item;
 import org.example.backend.model.ShoppingList;
+import org.example.backend.model.UpdateRequest;
 import org.example.backend.repository.ListRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -42,4 +45,29 @@ public class ShopService {
     public Optional<ShoppingList> getListById(String id) {
         return listRepo.findById(id);
     }
-}
+
+    public void updateItemDone(String listId, String itemName, UpdateRequest updateRequest) {
+        Optional<ShoppingList> optionalShoppingList = listRepo.findById(listId);
+        if (optionalShoppingList.isPresent()) {
+            ShoppingList shoppingList = optionalShoppingList.get();
+            boolean itemFound = false;
+
+            for (int i = 0; i < shoppingList.item().size(); i++) {
+                Item item = shoppingList.item().get(i);
+                if (item.name().equals(itemName)) {
+                    item = item.withDone(updateRequest.done());
+                    shoppingList.item().set(i, item);
+                    itemFound = true;
+                    break;
+                }
+            }
+            if (!itemFound) {
+                throw new RuntimeException("Item not found: " + itemName);
+            }
+            listRepo.save(shoppingList);
+        } else {
+            throw new RuntimeException("ShoppingList not found: " + listId);
+        }
+    }
+
+    }
